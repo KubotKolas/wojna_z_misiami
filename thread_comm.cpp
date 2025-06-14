6d04fc4618c707dd42a32edba5cfdab6e37fe849
KubotKolas <ben10gapa7098@gmail.com>
2025-06-14 15:38:51 +0200

    update

--- thread_comm.cpp
+++ thread_comm.cpp
@@ -14,59 +14,9 @@ void *startKomWatek(void *ptr)
     while ( stan!=FINISHED ) {
 	debug("czekam na recv");
         MPI_Recv( &pakiet, 1, MPI_PAKIET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        timer = std::max(pakiet.ts, timer) + 1;
-
-        switch ( pakiet.tag ) {
-	    case REQ_DOCK: 
-            // TODO: test
-            dock_requests.push({pakiet.ts, pakiet.src});
-            switch (stan) {
-                case IDLE:
-                case AWAIT_MECH:
-                case AWAIT_DOCK:
-                    if (dock_counter > 0 && !dock_requests.empty()){
-                        int dest = dock_requests.top().second;
-                        sendDock(dest, dock_counter);
-                        dock_requests.pop();
-                        dock_counter = 0;
-                    }
-                    break;
-                default:
-                    break;
-            }
-	        break;
-	    case REQ_MECH: 
-            // TODO: test
-            mech_requests.push({pakiet.ts, pakiet.src});
-            switch (stan) {
-                case IDLE:
-                case AWAIT_MECH:
-                    if (mech_counter > 0 && !mech_requests.empty()){
-                        int dest = mech_requests.top().second;
-                        sendDock(dest, mech_counter);
-                        mech_requests.pop();
-                        mech_counter = 0;
-                    }
-                    break;
-                default:
-                    break;
-            }
-	        break;
-        case T_DOCK:
-            // TODO: test
-            dock_counter += pakiet.data;
-            break;
-        case T_MECH:
-            // TODO: test
-            mech_counter += pakiet.data;
-            break;
-	    default:
-            // Unknown message type; throw an error
-            // TODO: to implement
-            debug("[!] [id:%d] Malformed request from %d!", tid , status.MPI_SOURCE)
-
-	        break;
-        }
+        handleMess(&pakiet, &status);
+        MPI_Bcast(&pakiet, 1, MPI_PAKIET_T, rank, MPI_COMM_WORLD);
+        handleMess(&pakiet, &status);
     }
     return nullptr;
 }
@@ -91,4 +41,60 @@ void sendMech(int dest, int n){
 
 void sendDock(int dest, int n){
 
+}
+
+void handleMess(packet* pakiet, MPI_Status* status){
+    timer = std::max(pakiet->ts, timer) + 1;
+
+        switch ( pakiet->tag ) {
+	    case REQ_DOCK: 
+            // TODO: test
+            dock_requests.push({pakiet->ts, pakiet->src});
+            switch (stan) {
+                case IDLE:
+                case AWAIT_MECH:
+                case AWAIT_DOCK:
+                    if (dock_counter > 0 && !dock_requests.empty()){
+                        int dest = dock_requests.top().second;
+                        sendDock(dest, dock_counter);
+                        dock_requests.pop();
+                        dock_counter = 0;
+                    }
+                    break;
+                default:
+                    break;
+            }
+	        break;
+	    case REQ_MECH: 
+            // TODO: test
+            mech_requests.push({pakiet->ts, pakiet->src});
+            switch (stan) {
+                case IDLE:
+                case AWAIT_MECH:
+                    if (mech_counter > 0 && !mech_requests.empty()){
+                        int dest = mech_requests.top().second;
+                        sendDock(dest, mech_counter);
+                        mech_requests.pop();
+                        mech_counter = 0;
+                    }
+                    break;
+                default:
+                    break;
+            }
+	        break;
+        case T_DOCK:
+            // TODO: test
+            dock_counter += pakiet->data;
+            break;
+        case T_MECH:
+            // TODO: test
+            mech_counter += pakiet->data;
+            break;
+	    default:
+            // Unknown message type; throw an error
+            // TODO: to implement
+            debug("[!] [id:%d] Malformed request from %d!", rank , status->MPI_SOURCE)
+
+	        break;
+        }
 }
--- thread_comm.hpp
+++ thread_comm.hpp
@@ -10,3 +10,5 @@ void requestDock();
 void sendMech(int dest, int n=1);
 
 void sendDock(int dest, int n=1);
+
+void handleMess(packet* p, MPI_Status* status);
